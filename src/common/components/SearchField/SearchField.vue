d26da6a5dc51e632a423e3786780168b8e9d476e
Wellington Barbosa <wba@cin.ufpe.br>
2021-08-28 21:55:48 -0300

    [MP-32] Fix search field component

--- src/common/components/SearchField/SearchField.vue
+++ src/common/components/SearchField/SearchField.vue
@@ -60,13 +60,13 @@ export default {
   },
   computed: {
     textFieldClass: function () {
-      const classes = ['text-field', `text-field--${this.align}`, 'search-field']
-      if (this.light) classes.push('text-field--light')
-      else classes.push('text-field--dark')
+      const classes = ['search-field', `search-field--${this.align}`]
+      if (this.light) classes.push('search-field--light')
+      else classes.push('search-field--dark')
       return classes
     },
     textFieldInputClass: function () {
-      return ['text-field__content', `text-field__content--${this.align}`]
+      return ['search-field__content', `search-field__content--${this.align}`]
     }
   },
   methods: {
@@ -87,8 +87,92 @@ export default {
 </script>
 
 <style lang="scss">
+.search-field {
+  font-family: 'customfont600';
+  color: var(--utilities-alternate);
+  border-radius: 15px;
+
+  &__content {
+    font-family: 'customfont700';
+    font-weight: 500;
+    color: var(--main-alternate);
+    font-size: var(--font-size-4);
+    margin-top: clamp(-15px, -10%, -1px);
+  }
+
+  &__content--center {
+    text-align: center !important;
+    padding-left: 0 !important;
+  }
+
+  &--center > .q-field__inner {
+    text-align: center !important;
+  }
+}
+
 .search-field.q-field--filled .q-field__control,
 .search-field.q-field--outlined .q-field__control {
   padding: 0 var(--spacing-2) 0 var(--spacing-4);
+}
+
+.q-field {
+  font-size: var(--font-size-3);
+
+  &__marginal {
+    color: var(--utilities-icon);
+  }
+
+  &__inner {
+    text-align: left !important;
+  }
+
+  &__label {
+    font-family: 'customfont600';
+    font-size: var(--font-size-4) !important;
+  }
+
+  &__bottom {
+    font-family: 'customfont600';
+    font-size: var(--font-size-3) !important;
+  }
+
+  &--filled &__control {
+    border-radius: var(--spacing-2);
+    padding: 0 var(--spacing-4);
+  }
+
+  &--filled &__control:after {
+    height: 0px !important;
+  }
+
+  &--filled &__control:before {
+    border-bottom: 0px !important;
+  }
+
+  &--outlined &__control {
+    border-radius: var(--spacing-1);
+    border: 2px solid var(--utilities-disabled);
+    padding: 0 var(--spacing-4);
+  }
+
+  &--error &__bottom {
+    color: var(--status-danger) !important;
+  }
+
+  &--highlighted &__control:before {
+    background: rgba(0, 0, 0, 0) !important;
+  }
+}
+
+.text-negative {
+  color: var(--status-danger) !important;
+}
+
+.text-field--light.q-field--filled .q-field__control {
+  background: var(--main-background);
+}
+
+.text-field--dark.q-field--filled .q-field__control {
+  background: var(--utilities-border);
 }
 </style>
