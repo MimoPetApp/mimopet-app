35056bddf83e95c9b657feb045c8eeb05e5ead52
Wellington Barbosa <wba@cin.ufpe.br>
2021-09-11 12:58:58 -0300

    Add Base Modal component

---
+++ src/common/components/Modal/BaseModal/BaseModal.vue
@@ -0,0 +1,25 @@
+<template>
+  <QDialog v-bind="$attrs" class="base-modal" full-width>
+    <q-card class="base-modal__card">
+      <slot></slot>
+    </q-card>
+  </QDialog>
+</template>
+
+<script>
+import { QDialog } from 'quasar'
+export default {
+  name: 'BaseModal',
+  components: { QDialog },
+  props: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.base-modal {
+  &__card {
+    background: var(--main-background);
+    border-radius: var(--spacing-3) !important;
+  }
+}
+</style>
