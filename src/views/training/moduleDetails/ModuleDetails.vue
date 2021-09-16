deaaa559a4c2c0caa6940bdcac35b21417951e95
chiuyong <yongcchiu@gmail.com>
2021-09-16 03:08:34 -0300

    module details view

--- src/views/training/moduleDetails/ModuleDetails.vue
+++ src/views/training/moduleDetails/ModuleDetails.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="training-details-wrapper">
-    <div v-if="true" class="training-details-wrapper__header">aa</div>
+  <div class="module-details-wrapper">
+    <div v-if="true" class="module-details-wrapper__header">
+      <h2 class="module-details-wrapper__header__title text-main-alternate pl-3 pr-3">
+        Equipamentos de passeio
+      </h2>
+      <q-separator class="mb-4" />
+      <h3 class="module-details-wrapper__header__subtitle text-main-alternate pl-3 pr-3">
+        10 Passos
+      </h3>
+      <h4 class="module-details-wrapper__header__caption text-utilities-alternate pl-3 pr-3 mb-4">
+        10-20 min cada
+      </h4>
+    </div>
     <div v-else class="flex flex-center q-mt-xl">
       <LoadingCircle color="status-waiting" size="6em" :thickness="5" />
     </div>
-    <!-- <CardHealth/> -->
   </div>
 </template>
 
