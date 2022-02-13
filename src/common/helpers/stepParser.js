e59869bc4be676f3e68efc836002c73db83c8645
chiuyong <yongcchiu@gmail.com>
2022-02-13 17:18:33 -0300

    fix prop access id

--- src/common/helpers/stepParser.js
+++ src/common/helpers/stepParser.js
@@ -1,27 +1,27 @@
 export default {
-  getStepRouter(step) {
+  getStepRouter (step) {
     let name = ''
     let id = 0
     switch (step.type) {
       case 'slide':
         name = 'SlideStep'
-        id = step.slides.id
+        id = step.id
         break
       case 'video':
         name = 'VideoStep'
-        id = step.video.id
+        id = step.id
         break
       case 'feedback':
         name = 'FeedbackStep'
-        id = step.feedback.id
+        id = step.id
         break
       case 'exercise':
         name = 'ExerciseStep'
-        id = step.exercise.id
+        id = step.id
         break
       case 'quiz':
         name = 'QuizStep'
-        id = step.quizzes.id
+        id = step.id
         break
       default:
         break
