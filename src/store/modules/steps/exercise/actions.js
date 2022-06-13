3a08478b32a7b28710986e5238a91b4fe056f9bc
chiuyong <yongcchiu@gmail.com>
2022-06-13 00:48:49 -0300

    [DEV-370] support for obendience

--- src/store/modules/steps/exercise/actions.js
+++ src/store/modules/steps/exercise/actions.js
@@ -5,10 +5,13 @@ import { Notify } from 'quasar'
 
 export default {
   async ActionUpdateExerciseStepCompleted ({ commit }, payload) {
+    // support exercise completed from trainings or obediences
+    let { url, body } = payload
     commit(types.LOADING_STEP_COMPLETED)
 
     try {
-      const response = await Http.put(`exercises/${payload.id}/complete`, payload.body)
+      //const response = await Http.put(`exercises/${payload.id}/complete`, payload.body)
+      const response = await Http.put(`${url}`, body)
       commit(types.SUCCESS_STEP_COMPLETED, response.data)
       return true
     } catch (error) {
